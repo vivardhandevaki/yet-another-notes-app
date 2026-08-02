72585663e06269eb46a15a5408bce793e22abf6b
vivardhandevaki <vivardhan.devaki@gmail.com>
2026-08-02 09:56:23 +0530

    Scaffold Spring Boot notes app

---
+++ src/main/java/com/vivardhandevaki/notes/YetAnotherNotesAppApplication.java
@@ -0,0 +1,12 @@
+package com.vivardhandevaki.notes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YetAnotherNotesAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YetAnotherNotesAppApplication.class, args);
+    }
+}
---
+++ src/test/java/com/vivardhandevaki/notes/YetAnotherNotesAppApplicationTests.java
@@ -0,0 +1,12 @@
+package com.vivardhandevaki.notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YetAnotherNotesAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
